5eab1c92d46432d39a8df165c3cb6d41ab7f0b91
Sebastian J. Ferrara <sjferrara@gmail.com>
2022-01-19 23:01:58 -0300

    exercise 02 repeat string resolved

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(str, n) {
+    if (n < 0) {
+        return 'ERROR';
+    }
+    let ret = '';
+    while (n > 0) {
+        ret += str;
+        n--;
+    }
+    return ret;
 };
 
 // Do not edit below this line
