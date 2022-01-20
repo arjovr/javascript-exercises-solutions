8cdfda55f5c3b89d5911766dbb7f225c89add326
Sebastian J. Ferrara <sjferrara@gmail.com>
2022-01-19 23:04:56 -0300

    exercise 07 tempConversions resolved

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(x) {
+    return Math.round((5 * (x - 32) / 9) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(x) {
+    return Math.round((9 * x / 5 + 32) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof
 };
