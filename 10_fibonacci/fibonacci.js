66a4902c6ae2de8a9f661c92abf1f3fa11c93507
Sebastian Ferrara <sebastian.ferrara@r2software.net>
2022-01-20 17:56:09 -0300

    10 fibonacci resolved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    if (x == 1) {
+        return 1;
+    }
+    if (x == 0) {
+        return 0;
+    }
+    if (x < 0) {
+        return 'OOPS';
+    }
+    return fibonacci(x-1) + fibonacci(x-2);
 };
 
 // Do not edit below this line
