b1da9f5cfda1b30e98941d090ae8973f39e1f685
Sebastian J. Ferrara <sjferrara@gmail.com>
2022-01-19 23:03:43 -0300

    exercise 05 sumAll resolved

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const euler = function(n) {
+    return n * (n + 1) / 2;
+}
 
+const sumAll = function(start, end) {
+    if (start < 0 || end < 0 || Number(start) !== start || Number(end) !== end) {
+        return 'ERROR';
+    }
+    let min = Math.min(start, end);
+    let max = Math.max(start, end);
+    return euler(max) - euler(min) + min;
 };
 
 // Do not edit below this line
