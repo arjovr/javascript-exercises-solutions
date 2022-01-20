2049c789c19452762d562693bb37876b331ed5c0
Sebastian J. Ferrara <sebastian.ferrara@r2software.net>
2022-01-19 23:44:58 -0300

    09 palindromes resolved

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,9 @@
-const palindromes = function () {
-
+const palindromes = function(x) {
+    x = x.replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g, '').toLowerCase();
+    const a = x.split('');
+    const b = [...a];
+    b.reverse();
+    return JSON.stringify(a) == JSON.stringify(b);
 };
 
 // Do not edit below this line
