4ea1399cb3ae84eb115b4a1aa8ab32740fb597ad
Sebastian J. Ferrara <sjferrara@gmail.com>
2022-01-19 23:03:15 -0300

    exercise 04 remove from array resolved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(xs, ...other) {
+    for (let i = 0; i < xs.length; i++) {
+        const x = xs[i];
+        if (other.includes(x)) {
+            xs.splice(i, 1);
+            i--;
+        }
+    }
+    return xs;
 };
 
 // Do not edit below this line
