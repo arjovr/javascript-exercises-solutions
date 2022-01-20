b39d7259095ce1604edb21998af8fbb3ef9fcd25
Sebastian Ferrara <sebastian.ferrara@r2software.net>
2022-01-20 19:18:03 -0300

    12 find the oldest resolved

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, p) => {
+        if (!oldest) {
+            return p;
+        }
+        const years = (p.yearOfDeath || (new Date()).getFullYear()) - p.yearOfBirth;
+        const yearsOldest = (oldest.yearOfDeath || (new Date()).getFullYear()) - oldest.yearOfBirth;
+        if (years > yearsOldest) {
+            return p;            
+        }
+        return oldest;
+    }, null);
 };
 
 // Do not edit below this line
